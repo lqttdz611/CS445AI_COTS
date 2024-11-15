95a7379262b8887609f35dd41ee2b890df321f85
lqttdz611 <lqttinh0611@gmail.com>
2024-11-15 14:35:46 +0700

    setting admin code

--- source_code/frontend/src/pages/ProductDetails/RelatedProduct/index.js
+++ source_code/frontend/src/pages/ProductDetails/RelatedProduct/index.js
@@ -1,20 +1,58 @@
-import React from 'react'
+import React from "react";
 
+import Rating from '@mui/material/Rating';import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
+// import { productData } from "../../../static/data";
+import { Link } from "react-router-dom";
 const RelatedProduct = () => {
   return (
     <>
-    <div className='d-flex align-items-center mt-3 card-body'>
-      <div className='mt-4 mb-2 text-center'>
-      <h4>Related Products</h4>
-      <p class="card-text">People also search for this items</p>
-      </div>
+      <div className=" card-body">
+        <div className="mt-4 mb-2 text-center">
+          <h4>Related Products</h4>
+          <p class="card-text">People also search for this items</p>
+        </div>
 
-      <div className='w-100 mt-0'>
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={30}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <div className="productItem">
+              <div className="imgWrapper">
+                <Link to = '/'>
+                <div className="item-heading">
+                  <h5 className="text-text-truncate mb-0">
+                  Apple MacBook Pro - Silver
+                  </h5>
+                  <small className="text-body">by Apple</small>
+                </div>
+                <div className="img-container w-50 mx-auto py-50">
+                <img src="https://i.pinimg.com/originals/8d/19/df/8d19df0f1d0347d9dec351afa2554106.jpg" className="w-100"></img>
 
+                </div>
+                <Rating name="read-only" value={4} readOnly />
+                <p className="card-text text-primary mb-0">$2449.49</p>
+                
+                </Link>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
+        <div className="w-100 mt-0"></div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default RelatedProduct
+export default RelatedProduct;
---
+++ source_code/server/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
+
+app.use(cors());
+app.options("*", cors());
+
+// middleware
+app.use(bodyParser.json());
+
+// Routes
+const categoryRoutes = require('./routes/categories')
+const productRoutes = require('./routes/products')
+
+app.use(`/api/category`, categoryRoutes)
+app.use(`/api/products`, productRoutes)
+
+// Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Database Connection is ready...')
+  // Server
+app.listen(process.env.PORT, ()=> {
+  console.log(`Server is running at https://localhost:${process.env.PORT}`)
+})
+}).catch((err) => {
+  console.log(err)
+})
